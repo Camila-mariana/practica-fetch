432b5dd168974d4a2d09c882e3e346c441308102
Camila <20240059@ricaldone.edu.sv>
2026-04-20 18:59:05 -0600

    Ya terminado

---
+++ practica-fetch/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <main className='container mx-auto flex h-screen flex-col items-center justify-center p-4 w-full'>
+            <h1 className='text-center'>Home</h1>
+            <p>Proyecto consumiendo APIs externas</p>
+        </main>
+    );
+}
+export default Home;
